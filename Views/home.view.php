e2b5158f2b905b074eed0b8d95a86cd71d757e4a
agodin3z <pistilloagodinez@gmail.com>
2018-04-15 17:35:12 -0600

    HomePage View agregada

---
+++ Views/home.view.php
@@ -0,0 +1,23 @@
+<?php
+
+  /**
+  * The home page view
+  */
+  class HomeView {
+
+      private $model;
+
+      private $controller;
+
+
+      function __construct($controller, $model) {
+          $this->controller = $controller;
+
+          $this->model = $model;
+      }
+
+      public function index() {
+          return $this->controller->loadView();
+      }
+
+  }
